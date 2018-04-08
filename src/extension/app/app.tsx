8360e8271d85af1d72b6d355a312b8aed896e717
grievouz <ronke.tammo@gmail.com>
2018-04-08 17:39:31 +0200

    Now everything is pretty, thanks to Prettier :white_flower:

--- src/extension/app/app.tsx
+++ src/extension/app/app.tsx
@@ -3,29 +3,29 @@ import { render } from "react-dom";
 import * as Browserium from "../../utils/Browserium";
 
 class App extends React.Component {
+    private iframeCss: React.CSSProperties = {
+        zIndex: 10000000,
+        position: "fixed",
+        top: "0",
+        right: "0",
+        border: "none",
+        background: "white",
+        height: "100vh",
+        width: "360px"
+    };
 
-
-	private iframeCss: React.CSSProperties = {
-		zIndex: 10000000,
-		position: "fixed",
-		top: "0",
-		right: "0",
-		border: "none",
-		background: "white",
-		height: "100vh",
-		width: "360px"
-	};
-
-	render() {
-		return (
-			<div id={"injected-react-app"}>
-				<iframe
-					src={Browserium.environment().runtime.getURL("/sidebar.html")}
-					style={this.iframeCss}
-				/>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div id={"injected-react-app"}>
+                <iframe
+                    src={Browserium.environment().runtime.getURL(
+                        "/sidebar.html"
+                    )}
+                    style={this.iframeCss}
+                />
+            </div>
+        );
+    }
 }
 let base: HTMLDivElement = document.createElement("div");
 base.id = "injected-react-base";
--- src/sidebar/app/components/pages/pages.tsx
+++ src/sidebar/app/components/pages/pages.tsx
@@ -5,42 +5,44 @@ import { observer } from "mobx-react";
 import Store from "./store";
 import * as SortBy from "../sortBy/sortBy";
 
-interface States{
-	pagesCss: React.CSSProperties;
+interface States {
+    pagesCss: React.CSSProperties;
 }
 
 @observer
 class Pages extends React.Component {
+    constructor(props) {
+        super(props);
 
-    constructor(props){
-		super(props);
+        this.state = {
+            pagesCss: {}
+        };
+    }
 
-		this.state = {
-			pagesCss: {}
-		}
-	}
-
-	render() {
-		return (
-			<div id="pages" style={Store.css} >
-				<div key={"pages-page-key-recent"} id={"pages-page-recent"} className={"pages-page-con"} >	
-					{Store.recentCards}
-				</div>
-				<div key={"pages-page-key-bookmarks"} id={"pages-page-bookmarks"} className={"pages-page-con"} >
-					<SortBy.default 
-						defaultSelector={"test2"} 
-						options={[
-							"Time",
-							"Categories",
-							"Tags",
-							"Groups"
-						]}
-					/>
-					{Store.bookmarkBtns}
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div id="pages" style={Store.css}>
+                <div
+                    key={"pages-page-key-recent"}
+                    id={"pages-page-recent"}
+                    className={"pages-page-con"}
+                >
+                    {Store.recentCards}
+                </div>
+                <div
+                    key={"pages-page-key-bookmarks"}
+                    id={"pages-page-bookmarks"}
+                    className={"pages-page-con"}
+                >
+                    <SortBy.default
+                        defaultSelector={"test2"}
+                        options={["Time", "Categories", "Tags", "Groups"]}
+                    />
+                    {Store.bookmarkBtns}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Pages;
